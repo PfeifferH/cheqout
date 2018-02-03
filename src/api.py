314d1414d26e07e1eba81ca6ec67713f49afa280
Ian <ianw3214@gmail.com>
2018-02-03 03:46:27 -0500

    Implement firestore initialization function

---
+++ src/api.py
@@ -0,0 +1,38 @@
+import uuid
+import datetime
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+def init(cart=None):
+    """
+    Initializes firestore document for a certain cart
+    @param cart: The cart to find the firestore document for, creates a new one if it is None
+    @return: A firestore Document object that references the specified cart
+
+    The function returns None if the request couldn't be fulfulled
+    """
+    cred = credentials.Certificate(
+        "../keys/cheqout-57ee7-firebase-adminsdk-8b1oa-8dd14d0e11.json")
+    firebase_admin.initialize_app(cred)
+
+    # App is an app object of sorts
+    App = firebase_admin.initialize_app(cred, None, 'Cheqout')
+    # Client is a firestore client, the docs is https://googlecloudplatform.github.io/google-cloud-python/latest/firestore/client.html
+    Client = firebase_admin.firestore.client()
+
+    # get a reference to the top level collection
+    collection = Client.collection('carts')
+
+    # if cart is not specified, create a new cart
+    if cart is None:
+        return collection.add({"id": str(uuid.uuid4()), "state": "inactive", "activated time": None, "items": []})
+    # otherwise, look for the document referencing the cart and return the Document object
+    else:
+        for document in collection.get():
+            if 'id' in document.to_dict() and document.to_dict()['id'] == cart:
+                return document
+        return None
+    
+    return None
---
+++ src/test.py
@@ -0,0 +1,4 @@
+import api
+
+if __name__=="__main__":
+    api.init("94769840-8d2a-440e-af83-816b69b75051")
