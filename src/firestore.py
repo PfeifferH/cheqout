ef9d4e1b6bf880e2c1ddb4cc7f7196cfd0fbab95
Ian <ianw3214@gmail.com>
2018-02-03 03:28:32 -0500

    setup file for firestore interfacing

---
+++ src/firestore.py
@@ -0,0 +1,17 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate(
+    "../keys/cheqout-57ee7-firebase-adminsdk-8b1oa-8dd14d0e11.json")
+firebase_admin.initialize_app(cred)
+
+# App is an app object of sorts
+App = firebase_admin.initialize_app(cred, None, 'Cheqout')
+# Client is a firestore client, the docs is https://googlecloudplatform.github.io/google-cloud-python/latest/firestore/client.html
+Client = firebase_admin.firestore.client()
+
+# the top level collection
+collection = Client.collection('carts')
+# gets the documents in the collection
+document = collection.add({"test": "1234"})
