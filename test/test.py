311469cd6ccdefe450d1d3aee2accbdc6f0f94d2
Ian <ianw3214@gmail.com>
2018-02-03 02:44:01 -0500

    Add super simple comments

--- test/test.py
+++ test/test.py
@@ -5,5 +5,7 @@ from firebase_admin import firestore
 cred = credentials.Certificate("../keys/cheqout-57ee7-firebase-adminsdk-8b1oa-8dd14d0e11.json")
 firebase_admin.initialize_app(cred)
 
+# App is an app object of sorts
 App = firebase_admin.initialize_app(cred, None, 'Cheqout')
+# Client is a firestore client, the docs is https://googlecloudplatform.github.io/google-cloud-python/latest/firestore/client.html
 Client = firebase_admin.firestore.client()
