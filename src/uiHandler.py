e226b38bc550fd2af4ecbfbdf86d6829db54f7ff
PfeifferH <15hp19@queensu.ca>
2018-02-04 00:50:40 -0500

    Started on Produce Processing

--- src/uiHandler.py
+++ src/uiHandler.py
@@ -4,13 +4,12 @@ sys.path.append('../cheqout/src')
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
-from client import *
+from client import Client
 #from barcode_detect import *
 
 from cart_interface import Ui_MainWindow
 from cart_interface_scan import Ui_ScanWindow
 from cart_interface_produce import Ui_produceWindow
-from cart_interface_items import Ui_ItemWindow
 
 import atexit
 
@@ -27,27 +26,23 @@ class ApplicationWindow(QMainWindow):
     def scanClick(self):
         # newItem = get_barcode(self)
         # add_item(self.cart, newItem)
-        add_item(self.cart, 'Tri-team-members')
+        Client.add_item(self.cart, 'Tri-team-members')
         self.StackedLayout.setCurrentIndex(1)
 
     def produceClick(self):
         self.StackedLayout.setCurrentIndex(2)
-
-    def itemsClick(self):
-        self.StackedLayout.setCurrentIndex(3)
-        print(get_items(self.cart))
 
 
     def mainClick(self):
         self.StackedLayout.setCurrentIndex(0)
 
     def all_done(self):
-        deactivate(self.cart)
+        Client.deactivate(self.cart)
 
     def __init__(self):
         # Set up api
-        self.cart = init("keys/cheqout-57ee7-firebase-adminsdk-8b1oa-8dd14d0e11.json", 'ULtXMhOuqcRHPpa2aKy1')
-        activate(self.cart)
+        self.cart = Client("keys/cheqout-57ee7-firebase-adminsdk-8b1oa-8dd14d0e11.json", 'ULtXMhOuqcRHPpa2aKy1')
+        Client.activate(self.cart)
 
 
         super(ApplicationWindow, self).__init__()
@@ -67,13 +62,8 @@ class ApplicationWindow(QMainWindow):
         produceUi = Ui_produceWindow()
         produceUi.setupUi(produce)
 
-        items = QMainWindow()
-        itemUi = Ui_ItemWindow()
-        itemUi.setupUi(items)
-
         self.StackedLayout.addWidget(scans)
         self.StackedLayout.addWidget(produce)
-        self.StackedLayout.addWidget(items)
         self.MainWidget = QWidget()
         self.MainWidget.setLayout(self.StackedLayout)
         self.setCentralWidget(self.MainWidget)
@@ -81,16 +71,23 @@ class ApplicationWindow(QMainWindow):
 
         ui.pushButton.clicked.connect(self.scanClick)
         ui.pushButton_2.clicked.connect(self.produceClick)
-        ui.pushButton_3.clicked.connect(self.itemsClick)
+        #ui.pushButton_3.clicked.connect(self.itemsClick)
 
         scansUi.pushButton_2.clicked.connect(self.mainClick)
 
         produceUi.pushButton_2.clicked.connect(self.mainClick)
 
-        itemUi.pushButton_2.clicked.connect(self.mainClick)
-        for i in range(len(get_items(self.cart))):
-            cartItem = QListWidgetItem(get_items(self.cart)[i]['id'])
-            itemUi.listWidget.addItem(cartItem)
+        priceTotal = 0
+        for i in range(len(Client.get_items(self.cart))):
+            priceTotal+= Client.get_items(self.cart)[i]['price']
+            cartItem = QListWidgetItem(Client.get_items(self.cart)[i]['name'] + " " + str(Client.get_items(self.cart)[i]['price']))
+            ui.listWidget.addItem(cartItem)
+
+        cartItem = QListWidgetItem("TOTAL PRICE: " + str(priceTotal))
+        ui.listWidget.addItem(cartItem)
+
+
+
 
         atexit.register(self.all_done)
 
