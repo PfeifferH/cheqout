4b53a6d21d91b01cd844c5c11b98df7dc24f77f8
wangz304 <ew98@foxmail.com>
2018-02-04 02:05:56 -0500

    update test button

--- src/test_button.py
+++ src/test_button.py
@@ -5,14 +5,19 @@ red = 23
 yellow = 24
 green = 25
 
-GPIO.setup(red, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(yellow, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(green, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(red, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(yellow, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(green, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+pre_input = None
 
 while True:
-    if (GPIO.input(red)):
+    if (GPIO.input(red) and pre_input != 'red'):
         print("red")
-    elif (GPIO.input(yellow)):
+        pre_input = 'red'
+    elif (GPIO.input(yellow) and pre_input != 'yellow'):
         print("yellow")
-    elif (GPIO.input(green)):
+        pre_input = 'yellow'
+    elif (GPIO.input(green) and pre_input != 'green'):
         print("green")
+        pre_input = 'green'
