8d1de4442a049c06433a92deec2a8a23a87cc5ef
PfeifferH <15hp19@queensu.ca>
2018-02-04 05:37:15 -0500

    Made Barcode Wait Screen

--- src/payment_barcode_interface.py
+++ src/payment_barcode_interface.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'cart_interface.ui'
+# Form implementation generated from reading ui file 'payment_barcode_interface.ui'
 #
 # Created by: PyQt5 UI code generator 5.10
 #
@@ -8,45 +8,26 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-class Ui_MainWindow(object):
+class Ui_PayWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 480)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(50, 125, 300, 120))
-        font = QtGui.QFont()
-        font.setPointSize(16)
-        self.pushButton_2.setFont(font)
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_3.setGeometry(QtCore.QRect(50, 275, 300, 120))
-        font = QtGui.QFont()
-        font.setPointSize(16)
-        self.pushButton_3.setFont(font)
-        self.pushButton_3.setObjectName("pushButton_3")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(200, 20, 400, 91))
+        self.label.setGeometry(QtCore.QRect(175, 110, 450, 150))
         font = QtGui.QFont()
-        font.setPointSize(30)
+        font.setPointSize(16)
         self.label.setFont(font)
-        self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.label.setObjectName("label")
-        self.listWidget = QtWidgets.QListWidget(self.centralwidget)
-        self.listWidget.setGeometry(QtCore.QRect(450, 125, 300, 275))
-        self.listWidget.setObjectName("listWidget")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
         self.menubar.setObjectName("menubar")
-        self.menuCartHome = QtWidgets.QMenu(self.menubar)
-        self.menuCartHome.setObjectName("menuCartHome")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
-        self.menubar.addAction(self.menuCartHome.menuAction())
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
@@ -54,10 +35,8 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton_2.setText(_translate("MainWindow", "PRODUCE"))
-        self.pushButton_3.setText(_translate("MainWindow", "PAY"))
-        self.label.setText(_translate("MainWindow", "cheqout"))
-        self.menuCartHome.setTitle(_translate("MainWindow", "CartHome"))
+        self.label.setText(_translate("MainWindow", "Please Scan your Payment Barcode or \n"
+"proceed to a checkout"))
 
 
 if __name__ == "__main__":
