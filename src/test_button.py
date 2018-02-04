c5f3c70354f2c91373966c0a634b94437555f3a9
wangz304 <ew98@foxmail.com>
2018-02-04 02:10:51 -0500

    upadte test button

--- src/test_button.py
+++ src/test_button.py
@@ -9,15 +9,23 @@ GPIO.setup(red, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(yellow, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(green, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-pre_input = None
+pre_red = False
+pre_yellow = False
+pre_green = False
 
 while True:
-    if (GPIO.input(red) and pre_input != 'red'):
+    if GPIO.input(red) and not pre_red:
         print("red")
-        pre_input = 'red'
-    elif (GPIO.input(yellow) and pre_input != 'yellow'):
+        pre_red = True
+    else:
+        pre_red = False
+    if GPIO.input(yellow) and not pre_yellow:
         print("yellow")
-        pre_input = 'yellow'
-    elif (GPIO.input(green) and pre_input != 'green'):
+        pre_yellow = True
+    else:
+        pre_yellow = False
+    if GPIO.input(green) and not pre_green:
         print("green")
-        pre_input = 'green'
+        pre_green = True
+    else:
+        pre_green = False
