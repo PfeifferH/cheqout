b4ed3e952611bb514f07b1da2f174dcc513f4776
wangz304 <ew98@foxmail.com>
2018-02-04 07:15:33 -0500

    button programmed

--- src/uiHandler.py
+++ src/uiHandler.py
@@ -132,17 +132,17 @@ class ApplicationWindow(QMainWindow):
 
     def red_click(self):
         if layout_index == 0:
-            self.produceClick()
+            pass
         elif layout_index == 1:
             pass
         elif layout_index == 2:
-            pass
+            self.mainClick()
         else:
             pass
 
     def yellow_click(self):
         if layout_index == 0:
-            pass
+            self.payClick()
         elif layout_index == 1:
             pass
         elif layout_index == 2:
@@ -153,11 +153,11 @@ class ApplicationWindow(QMainWindow):
 
     def green_click(self):
         if layout_index == 0:
-            pass
+            self.produceClick()
         elif layout_index == 1:
-            pass
+            self.produceEnterClick()
         elif layout_index == 2:
-            pass
+            self.mainClick()
         else:
             pass
         self.produceEnterClick()
