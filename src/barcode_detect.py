d175c1f802ef31dbdde0a4cbf235b73aedd9d2a8
Ian <ianw3214@gmail.com>
2018-02-04 04:32:20 -0500

    Try a new barcode detection code snippet

--- src/barcode_detect.py
+++ src/barcode_detect.py
@@ -1,89 +1,153 @@
+# from picamera.array import PiRGBArray
+# from picamera import PiCamera
+# import time
+# import sys
+# import cv2
+# import zbar
+# from PIL import Image
+
+# def get_barcode(display=True):
+
+#     # Debug mode
+#     if len(sys.argv) > 1:
+#         display = sys.argv[-1] == 'display'
+
+#     # Configuration options
+#     RESOLUTION = (300, 160)
+
+#     # Initialise Raspberry Pi camera
+#     camera = PiCamera()
+#     camera.resolution = RESOLUTION
+#     #camera.framerate = 10
+#     camera.vflip = True
+#     camera.hflip = True
+#     #camera.color_effects = (128, 128)
+#     # set up stream buffer
+#     rawCapture = PiRGBArray(camera, size=RESOLUTION)
+#     # allow camera to warm up
+#     time.sleep(0.1)
+#     #print ("PiCamera ready")
+
+#     # Initialise OpenCV window
+#     if display:
+#         cv2.namedWindow("#cheqout")
+#         cv2.moveWindow("#chequout", 400, 160)
+
+#     #print ("OpenCV version: %s" % (cv2.__version__))
+#     #print ("Press q to exit ...")
+
+#     scanner = zbar.ImageScanner()
+
+#     first = None
+#     second = None
+#     third = None
+
+#     # Capture frames from the camera
+#     for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+#         # as raw NumPy array
+#         output = frame.array.copy()
+
+#         # raw detection code
+#         gray = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY, dstCn=0)
+#         pil = Image.fromarray(gray)
+#         width, height = pil.size
+#         raw = pil.tobytes()
+
+#         codes = scanner.scan(raw, width, height, 'Y800')
+
+#         if len(codes) > 0:
+#             if first is None:
+#                 first = codes
+#             elif second is None:
+#                 second = codes
+#             elif third is None:
+#                 third = codes
+        
+#         cv2.imshow("#cheqout", output)
+
+#         # clear stream for next frame
+#         rawCapture.truncate(0)
+
+#         # Wait for the magic key
+#         if display:
+#             keypress = cv2.waitKey(1) & 0xFF
+
+#         if third is not None:
+#             if first == second and second == third:
+#                 (third[0])
+#                 break;
+#             else:
+#                 first = None
+#                 second = None
+#                 third = None
+
+#     # When everything is done, release the capture
+#     camera.close()
+#     if display:
+#         cv2.destroyAllWindows()
+
+#     return third[0]
+
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import sys
 import cv2
 import zbar
-from PIL import Image
+import Image
 
 def get_barcode(display=True):
 
     # Debug mode
+    DEBUG = False
     if len(sys.argv) > 1:
-        display = sys.argv[-1] == 'display'
+        DEBUG = sys.argv[-1] == 'DEBUG'
 
     # Configuration options
-    RESOLUTION = (300, 160)
+    FULLSCREEN = not DEBUG
+    if not DEBUG:
+        RESOLUTION = (640, 480)
+    else:
+        RESOLUTION = (480, 270)
 
     # Initialise Raspberry Pi camera
     camera = PiCamera()
     camera.resolution = RESOLUTION
     #camera.framerate = 10
-    camera.vflip = True
-    camera.hflip = True
+    #camera.vflip = True
+    #camera.hflip = True
     #camera.color_effects = (128, 128)
     # set up stream buffer
     rawCapture = PiRGBArray(camera, size=RESOLUTION)
     # allow camera to warm up
     time.sleep(0.1)
-    #print ("PiCamera ready")
-
-    # Initialise OpenCV window
-    if display:
-        cv2.namedWindow("#cheqout")
-        cv2.moveWindow("#chequout", 400, 160)
-
-    #print ("OpenCV version: %s" % (cv2.__version__))
-    #print ("Press q to exit ...")
 
     scanner = zbar.ImageScanner()
-
-    first = None
-    second = None
-    third = None
+    scanner.parse_config('enable')
 
     # Capture frames from the camera
     for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
         # as raw NumPy array
         output = frame.array.copy()
 
-        # # raw detection code
-        # gray = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY, dstCn=0)
-        # pil = Image.fromarray(gray)
-        # width, height = pil.size
-        # raw = pil.tobytes()
+        # raw detection code
+        gray = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY, dstCn=0)
+        pil = Image.fromarray(gray)
+        width, height = pil.size
+        raw = pil.tostring()
 
-        # codes = scanner.scan(raw, width, height, 'Y800')
+        # create a reader
+        image = zbar.Image(width, height, 'Y800', raw)
+        scanner.scan(image)
 
-        # if len(codes) > 0:
-        #     if first is None:
-        #         first = codes
-        #     elif second is None:
-        #         second = codes
-        #     elif third is None:
-        #         third = codes
-        
-        # cv2.imshow("#cheqout", output)
+        # extract results
+        for symbol in image:
+            # do something useful with results
+            print ('decoded', symbol.type, 'symbol', '"%s"' % symbol.data)
 
-        # # clear stream for next frame
-        # rawCapture.truncate(0)
-
-        # # Wait for the magic key
-        # if display:
-        #     keypress = cv2.waitKey(1) & 0xFF
-
-        # if third is not None:
-        #     if first == second and second == third:
-        #         (third[0])
-        #         break;
-        #     else:
-        #         first = None
-        #         second = None
-        #         third = None
+        # clear stream for next frame
+        rawCapture.truncate(0)
 
     # When everything is done, release the capture
     camera.close()
-    if display:
-        cv2.destroyAllWindows()
-
-    return third[0]
+    cv2.destroyAllWindows()
